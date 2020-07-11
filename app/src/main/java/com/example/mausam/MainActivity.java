a27f2e74d57561988679482b22235c1f87150545
Vishal <vishaalbhardwaji8654@gmail.com>
2020-07-10 21:12:42 -0400

    minor change

--- app/src/main/java/com/example/mausam/MainActivity.java
+++ app/src/main/java/com/example/mausam/MainActivity.java
@@ -1,10 +1,24 @@
 package com.example.mausam;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+
+import com.example.mausam.adaptor.RecyclerViewAdaptor;
+
+import java.lang.reflect.Array;
+import java.util.*;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private RecyclerViewAdaptor recyclerViewAdaptor;
+    private ArrayList   cityList;
+    private ArrayAdapter arrayAdapter;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
